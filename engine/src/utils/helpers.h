c94dfdb4034cc4b463e5961cf028e1bd54e96af9
iitzfarooq <mfarooq102005@gmail.com>
2025-11-15 16:49:22 +0500

    feat: Overload operators for set operations

--- engine/src/utils/helpers.h
+++ engine/src/utils/helpers.h
@@ -5,6 +5,8 @@
 #include <utility>
 #include <string>
 #include <stdexcept>
+#include <algorithm>
+#include <set>
 
 inline void req(bool condition, const std::string& message) {
     if (!condition) {
@@ -16,3 +18,36 @@ template <typename... ts>
 struct overloaded : ts... {
     using ts::operator()...;
 };
+
+template <typename T>
+std::set<T> operator|(const std::set<T>& a, const std::set<T>& b) {
+    std::set<T> result;
+    std::set_union(
+        a.begin(), a.end(),
+        b.begin(), b.end(),
+        std::inserter(result, result.begin())
+    );
+    return result;
+}
+
+template <typename T>
+std::set<T> operator&(const std::set<T>& a, const std::set<T>& b) {
+    std::set<T> result;
+    std::set_intersection(
+        a.begin(), a.end(),
+        b.begin(), b.end(),
+        std::inserter(result, result.begin())
+    );
+    return result;
+}
+
+template <typename T>
+std::set<T> operator-(const std::set<T>& a, const std::set<T>& b) {
+    std::set<T> result;
+    std::set_difference(
+        a.begin(), a.end(),
+        b.begin(), b.end(),
+        std::inserter(result, result.begin())
+    );
+    return result;
+}
